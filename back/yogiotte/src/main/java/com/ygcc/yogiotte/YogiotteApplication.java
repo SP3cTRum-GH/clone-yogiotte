7f8cb071360aa6a4d292d91285bda1df158de3ad
SP3cTRum-GH <min1324354@gmail.com>
2025-07-09 14:27:01 +0900

    spring boot connect

---
+++ back/yogiotte/src/main/java/com/ygcc/yogiotte/YogiotteApplication.java
@@ -0,0 +1,13 @@
+package com.ygcc.yogiotte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiotteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogiotteApplication.class, args);
+	}
+
+}
---
+++ back/yogiotte/src/test/java/com/ygcc/yogiotte/YogiotteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygcc.yogiotte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogiotteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
