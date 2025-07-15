b979fdd9f5fed49e9fb6f832b5646047363babdd
강지현 <wlgus4083@gmail.com>
2025-07-16 00:27:12 +0900

    [css] domestic OverView infi-container CSS upload

--- front/src/domestic-page/OverView.js
+++ front/src/domestic-page/OverView.js
@@ -33,6 +33,32 @@ const OverView = () => {
                     <h2><span>183,903</span>원~</h2>
                 </div>
             </div>
+
+            <div className='info-container'>
+                <div className='review-card card'>
+                    <div className='rating'>
+                        <p>★9.1</p>
+                        <h2>18,515명 평가 {">"} </h2>
+                    </div>
+                    <p>체크인부터 체크아웃까지 전반적으로 정말 만족스러운 숙박 경험이었습니다. 객실은 생각보다 훨씬 넓고 쾌적했으며, ...</p>
+                </div>
+
+                <div>
+                    <div className='card'>
+                        <h2>서비스 및 부대시설 {">"} </h2>
+                    </div>
+                    <p>공항 이동 교통수단</p>
+                    <p>무료 Wi-Fi</p>
+                </div>
+
+                <div className='info-location'>
+                    <div className='card'>
+                        <h2>위치 정보 {">"} </h2>
+                    </div>
+                    <p>강원 강를시 강문동 1-1</p>
+                    <p>강릉 강문해변 앞</p>
+                </div>
+            </div>
         </>
     )
 }
