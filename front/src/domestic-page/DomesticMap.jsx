154f364d0aa4f28c4326399150eca183bcb61ba1
강지현 <wlgus4083@gmail.com>
2025-07-17 23:14:41 +0900

    [update] naver map API 기능 추가

---
+++ front/src/domestic-page/DomesticMap.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import "./DomesticMap.css";
+
+const DomesticMap = () => {
+  useEffect(() => {
+    // 주소 → 좌표 변환
+    window.naver.maps.Service.geocode(
+      { query: "강원 강릉시 강문동 1-1" },
+      (status, response) => {
+        if (status !== window.naver.maps.Service.Status.OK) {
+          return alert("주소 변환 실패");
+        }
+
+        const { x, y } = response.v2.addresses[0]; // x: 경도, y: 위도
+        const location = new window.naver.maps.LatLng(y, x);
+
+        const map = new window.naver.maps.Map("map", {
+          center: location,
+          zoom: 14,
+          scrollWheel: false, // 마우스 휠 줌 비활성화
+          draggable: true,
+          disableDoubleClickZoom: true,
+        });
+
+        new window.naver.maps.Marker({
+          position: location,
+          map: map,
+        });
+      }
+    );
+  }, []);
+
+  return (
+    <div className="domestic-map-container">
+      <h3>위치</h3>
+      <div id="map" className="domestic-map"></div>
+
+      <h2>강원 강릉시 강문동 1-1</h2>
+      <ul>
+        <li>강릉 강문해변 앞</li>
+        <li>경포대 차량 7분</li>
+        <li>오죽헌 차량 11분</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DomesticMap;
---
+++ front/src/domestic-page/Service.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Service.css";
+
+const Service = () => {
+  return (
+    <div className="service-wrap">
+      <div className="service-container">
+        <h1>서비스 및 부대시설</h1>
+        <div>
+          <p>피트니스</p>
+          <p>무선인터넷</p>
+          <p>금연</p>
+          <p>짐보관가능</p>
+          <p>드라이기</p>
+          <p>전기주전자</p>
+          <p>반려견동반</p>
+          <p>TV</p>
+          <p>샤워실</p>
+          <p>카드결제</p>
+          <p>커피머신</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
