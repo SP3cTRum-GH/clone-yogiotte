1345a475301ac1a6ad0876d88e9bbfc163262ff1
강지현 <wlgus4083@gmail.com>
2025-07-21 22:28:57 +0900

    [update] 숙소 card 전체보기 버튼 기능 추가

--- front/src/components/OverseasCard.jsx
+++ front/src/components/OverseasCard.jsx
@@ -1,69 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import "./OverseasCard.css";
 
 const OverseasCard = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll(true);
+  };
+
   return (
     <>
-      <div className="overseas-card">
-        <p>스탠다드 룸, 더블베드[금연]</p>
-        <div className="overseas-wrap">
-          <div>
-            <img
-              src="https://image.withstatic.com/39/336/57/d59adfec6ccb4564838e527cd53e69f1.jpg?ext=webp"
-              alt="숙소이미지"
-            />
-            <p>성인 2명</p>
-          </div>
-          <div className="overseas-container">
-            <div className="overseas-info">
-              <h4>
-                스탠다드 더블(금연) [얼리버드 30일전] 스탠다드 플로어 (룸온리)
-              </h4>
-
-              <div className="overseas-guide">
-                <div className="free-cancel">
-                  <span>무료취소 - 기간까지</span>
-                  <p>조식 불포함</p>
-                  <p>더블 베드 1개</p>
+      <div className={showAll ? "show-card" : "OverseasCard"}>
+        {[...Array(10)].map((_, i) => {
+          return (
+            <div className="overseas-card">
+              <p>스탠다드 룸, 더블베드[금연]</p>
+              <div className="overseas-wrap">
+                <div>
+                  <img
+                    src="https://image.withstatic.com/39/336/57/d59adfec6ccb4564838e527cd53e69f1.jpg?ext=webp"
+                    alt="숙소이미지"
+                  />
+                  <p>성인 2명</p>
                 </div>
+                <div className="overseas-container">
+                  <div className="overseas-info">
+                    <h4>
+                      스탠다드 더블(금연) [얼리버드 30일전] 스탠다드 플로어
+                      (룸온리)
+                    </h4>
 
-                <div className="overseas-price">
-                  <h4>
-                    144,153<span>원</span>
-                  </h4>
-                  <p>총 1박</p>
-                  <p>세금 및 봉사료 포함</p>
-                  <p>남은 객실 2개</p>
-                  <button>예약하기</button>
+                    <div className="overseas-guide">
+                      <div className="free-cancel">
+                        <span>무료취소 - 기간까지</span>
+                        <p>조식 불포함</p>
+                        <p>더블 베드 1개</p>
+                      </div>
+
+                      <div className="overseas-price">
+                        <h4>
+                          144,153<span>원</span>
+                        </h4>
+                        <p>총 1박</p>
+                        <p>세금 및 봉사료 포함</p>
+                        <p>남은 객실 2개</p>
+                        <button>예약하기</button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="overseas-info">
+                    <h4>
+                      스탠다드 더블(금연) [얼리버드 30일전] 스탠다드 플로어
+                      (룸온리)
+                    </h4>
+
+                    <div className="overseas-guide">
+                      <div className="free-cancel">
+                        <span>무료취소 - 기간까지</span>
+                        <p>조식 불포함</p>
+                        <p>더블 베드 1개</p>
+                      </div>
+
+                      <div className="overseas-price">
+                        <h4>
+                          144,153<span>원</span>
+                        </h4>
+                        <p>총 1박</p>
+                        <p>세금 및 봉사료 포함</p>
+                        <p>남은 객실 2개</p>
+                        <button>예약하기</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-
-            <div className="overseas-info">
-              <h4>
-                스탠다드 더블(금연) [얼리버드 30일전] 스탠다드 플로어 (룸온리)
-              </h4>
-
-              <div className="overseas-guide">
-                <div className="free-cancel">
-                  <span>무료취소 - 기간까지</span>
-                  <p>조식 불포함</p>
-                  <p>더블 베드 1개</p>
-                </div>
-
-                <div className="overseas-price">
-                  <h4>
-                    144,153<span>원</span>
-                  </h4>
-                  <p>총 1박</p>
-                  <p>세금 및 봉사료 포함</p>
-                  <p>남은 객실 2개</p>
-                  <button>예약하기</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
+      </div>
+      <div className="button-box">
+        <button
+          className={showAll ? "hiden-btn" : "show-btn"}
+          onClick={handleShowAll}
+        >
+          전체보기
+        </button>
       </div>
     </>
   );
